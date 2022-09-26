ffc53da4bc8ba92d0dc6c6508a5a3e9bd92c3fa8
musesum <musesum@gmail.com>
2022-09-26 15:14:06 -0700

    MuMidi integration

--- Sources/MuMenuSky/TestSkyTr3.swift
+++ Sources/MuMenuSky/TestSkyTr3.swift
@@ -13,8 +13,6 @@ public class TestSkyTr3: NSObject {
     }
 
     func parseScriptFiles() {
-
         _ = MuMenuSky.parseTr3(root, "menu")
-        // print(root.makeTr3Script(indent: 0, pretty: false))
     }
 }
